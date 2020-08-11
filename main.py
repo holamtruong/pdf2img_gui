f561327e035c9315df1918939b9019d4676948ad
Truong Ho <truong360@gmail.com>
2020-08-11 15:34:09 +0700

    first

---
+++ main.py
@@ -0,0 +1,22 @@
+from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+
+# pages = convert_from_path('input/file_a.pdf', output_folder='output', poppler_path='poppler/bin')
+
+"""
+images = convert_from_path('input/file_a.pdf', poppler_path='poppler/bin')
+for image in images:
+    image.save('output/sample1.png', 'PNG')
+"""
+
+images = convert_from_path('input/file_multi.pdf', poppler_path='poppler/bin')
+i = 1
+for image in images:
+    image.save('output/' + 'multi_' + str(i) + '.jpg', 'JPEG')
+    i = i + 1
+
+print("Done!")
