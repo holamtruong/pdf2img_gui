9dbcce999806859303a348f4d685f9bd5cdec764
Truong Ho <truong360@gmail.com>
2020-08-12 19:49:19 +0700

    panel

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ window = Tk()
 # Set window title
 window.title('PDF to IMAGE')
 # Set window size
-window.geometry("330x190")
+window.geometry("370x250")
 
 # Set root variable
 window.path_file_pdf = 'None'
@@ -84,12 +84,16 @@ window.file_name = 'None'
 
 # Create UI widgets
 
+canvas = Canvas(window, width=120, height=60)
+img = PhotoImage(file="media\panel.png")
+canvas.create_image(20, 0, anchor=NW, image=img)
+
 label_input = Label(window, text="Input:")
-label_input_path = Label(window, text="Select your PDF file", width=30, relief="groove", fg="blue")
+label_input_path = Label(window, text="Select your PDF file", width=30, relief="groove", fg="gray")
 button_browse_input = Button(window, text="Browse", command=browseFiles)
 
 label_output = Label(window, text="Output:")
-label_output_path = Label(window, text="Select your output folder", width=30, relief="groove", fg="blue")
+label_output_path = Label(window, text="Select your output folder", width=30, relief="groove", fg="gray")
 button_browse_output = Button(window, text="Browse", command=browseFolder)
 
 button_run = Button(window, text="Run", padx=30, pady=10, command=pdf2img)
@@ -106,29 +110,33 @@ combo = Combobox(window)
 combo['values'] = (100, 200, 300, 400, 500, 700, 1000)
 combo.current(1)  # set the selected item
 
+label_author = Label(window, text='  Ⓒ Truong Ho', fg="gray")
 label_app_status = Label(window, text='Welcome!', fg="green")
 
 # Grid layout setting
-label_input.grid(column=0, row=0)
-label_input_path.grid(column=1, row=0)
 
-button_browse_input.grid(column=2, row=0)
-label_output.grid(column=0, row=1)
+canvas.grid(column=1, row=0, padx=5, pady=5)
 
-label_output_path.grid(column=1, row=1)
-button_browse_output.grid(column=2, row=1)
+label_input.grid(column=0, row=1)
+label_input_path.grid(column=1, row=1)
+button_browse_input.grid(column=2, row=1)
 
-label_dpi.grid(column=0, row=2)
-combo.grid(column=1, row=2)
+label_output.grid(column=0, row=2)
+label_output_path.grid(column=1, row=2)
+button_browse_output.grid(column=2, row=2)
 
-label_type.grid(column=0, row=3)
-combo_type.grid(column=1, row=3)
+label_dpi.grid(column=0, row=3)
+combo.grid(column=1, row=3)
 
-button_run.grid(column=1, row=4, pady=5)
-button_about.grid(column=2, row=4)
+label_type.grid(column=0, row=4)
+combo_type.grid(column=1, row=4)
+
+button_run.grid(column=1, row=5, pady=5)
+button_about.grid(column=2, row=5)
 button_exit.grid(column=2, row=5)
 
-label_app_status.grid(column=1, row=5)
+label_author.grid(column=0, row=6)
+label_app_status.grid(column=1, row=6)
 
 # Let the window wait for any events
 window.mainloop()
